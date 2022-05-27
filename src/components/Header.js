6ade7c3e51706dd73370d1c5f2968efb9d691eb0
Nicole Calderón <48034184+nicolexlc@users.noreply.github.com>
2022-05-26 19:55:18 -0600

    add cart sub-page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
-import { Container, Dropdown, FormControl, Navbar, Nav, Badge } from "react-bootstrap";
+import { Container, Dropdown, FormControl, Navbar, Nav, Badge, Button } from "react-bootstrap";
 import { FaShoppingCart } from 'react-icons/fa';
+import { AiFillDelete } from 'react-icons/ai';
 import { Link } from "react-router-dom";
+import { CartState } from "../context/Context";
 
 const Header = () => {
+
+    const { 
+        state: {cart},
+        dispatch
+    } = CartState();
+
     return (
         <Navbar bg="dark" variant="dark" style={{ height: 80}}>
             <Container>
@@ -13,14 +21,48 @@ const Header = () => {
                     <FormControl style={{ width: 500}} placeholder='Search a product' className="m-auto"/>
                 </Navbar.Text>
                 <Nav>
-                    <Dropdown alignright="true">
+                    <Dropdown>
                         <Dropdown.Toggle variant="success">
                             <FaShoppingCart color="white" fontSize="25px" />
-                            <Badge bg="success">10</Badge>
+                            <Badge bg="success">{cart.length}</Badge>
                         </Dropdown.Toggle>
 
                         <Dropdown.Menu style={{ minWidth: 370 }}>
-                            <span style={{ padding: 10}}>Cart is Empty!</span>
+
+                            { cart.length>0?(
+                                <>
+                                {
+                                    cart.map(prod => (
+                                        <span className="cartitem" key={prod.id}>
+                                            <img
+                                                src={prod.image}
+                                                className="cartItemImg"
+                                                alt={prod.name}
+                                            />
+                                            <div className="cartItemDetail">
+                                                <span>{prod.name}</span>
+                                                <span>$ {prod.price.split(".")[0]}</span>
+                                            </div>
+                                            <AiFillDelete
+                                                fontSize="20px"
+                                                style={{ cursor: "pointer" }}
+                                                onClick={() =>
+                                                    dispatch({
+                                                        type: "REMOVE_FROM_CART",
+                                                        payload: prod,
+                                                    })
+                                                }
+                                            />
+                                        </span>
+                                    ))}
+                                    <Link to="/cart">
+                                        <Button style={{width: "95%", margin: "0 10px" }}>
+                                            Go to Cart
+                                        </Button>
+                                    </Link>
+                                </>
+                            ):(<span style={{ padding: 10}}>Cart is Empty!</span>)}
+                            
                         </Dropdown.Menu>
                     </Dropdown>
                 </Nav>
